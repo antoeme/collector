eed336b83a8320748ca65791b19dd982e37e47f4
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-07-21 13:03:58 +0200

    fatte chiamate verso i server flask

--- collector.py
+++ collector.py
@@ -1,29 +1,37 @@
+import json
 from flask import Flask,jsonify
 import requests
 
 GET_TEMP = "http://127.0.0.1:5000/temp"  #url del server per la get delle temperature
-GET_STATUS_RELAYS = "http://127.0.0.1:5000/status"
+GET_STATUS_RELAYS = "http://127.0.0.1:5001/status"
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def helloworld():
     return jsonify({"about": " Helloworld !"})
 
-# @app.route('/status_relays')
-# def get_status():
-#     response_status = requests.get(GET_STATUS_RELAYS)
-#     #print(response_status)
-#     return jsonify({"about" : response_status.json})
+@app.route('/status_relays')
+def get_status():
+    response_status = requests.get(GET_STATUS_RELAYS)
+    #print(response_status)
+    return jsonify(response_status.json())
 
-response = requests.get(GET_TEMP)
-print(response)
-print(response.json()) 
+@app.route('/temps')
+def get_temps():
+    response_temps = requests.get(GET_TEMP)
+    print(response_temps) 
+    return jsonify(response_temps.json()) #response.json è una lista, con jsonify diventa un json
+
+# response = requests.get(GET_STATUS_RELAYS)
+# print(response)
+# print(response.json()) 
     
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,port=1000)
+    app.run(host='0.0.0.0', debug=True,port=5002)
 
 
 
