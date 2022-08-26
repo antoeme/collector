6af34fd4f7b0ba14114d56490f0c614d203de6f0
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-26 13:22:29 +0200

    sistemate variabili d'ambiente e dockerfile

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import json
 from flask import Flask,jsonify
 import requests
+
 from os import getenv
+from dotenv import load_dotenv
+
 from requests.auth import HTTPBasicAuth
 
-GET_TEMP = getenv("GET_TEMP") or "http://127.0.0.1:5000/temp"  #url del server per la get delle temperature
-GET_STATUS_RELAYS = getenv("GET_STATUS_RELAYS") or "http://127.0.0.1:5001/status"
+GET_TEMP = getenv("GET_TEMP") #or "http://127.0.0.1:5000/temp"  #url del server per la get delle temperature
+GET_STATUS_RELAYS = getenv("GET_STATUS_RELAYS") #or "http://127.0.0.1:5001/status"
 username = "daniele" or "antonio"
 password = "Cisco123" or "Dtlab123"
+
+
 app = Flask(__name__)
 
+# load environment variables from '.env' file
+load_dotenv()
 
 @app.route('/status_relays')
 def get_status():
@@ -25,6 +32,7 @@ def get_temps():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True,port=5002)
+
 
 
 
